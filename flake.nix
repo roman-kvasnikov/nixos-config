8392ecb82c89d2fb69e1d10037e0c98d2ca541b6
Roman Kvasnikov <roman.kvasnikov@gmail.com>
2025-07-17 11:06:02 +0300

    24.11

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   inputs = {
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -14,10 +14,10 @@
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: let
     system = "x86_64-linux";
-    homeStateVersion = "25.11";
+    homeStateVersion = "24.11";
     user = "romank";
     hosts = [
-      { hostname = "nixos"; stateVersion = "25.11"; }
+      { hostname = "nixos"; stateVersion = "24.11"; }
     ];
 
     makeSystem = { hostname, stateVersion }: nixpkgs.lib.nixosSystem {
--- nixos/modules/gnome.nix
+++ nixos/modules/gnome.nix
@@ -1,6 +1,7 @@
 {
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   environment.systemPackages = with pkgs; [
     gnomeExtensions.blur-my-shell
