7b5172d78ec1074598756ac56af5246175939ab4
Roman Kvasnikov <roman.kvasnikov@gmail.com>
2025-07-17 16:00:28 +0300

    fix

--- nixos/modules/gnome.nix
+++ nixos/modules/gnome.nix
@@ -4,46 +4,46 @@
   services.xserver = {
     enable = true;
     displayManager.gdm.enable = true;
-    desktopManager.gnome = {
-      enable = true;
-      excludePackages = with pkgs.gnome; [
-        # Ненужные приложения
-        gnome-tour
-        gnome-music
-        gnome-photos
-        gnome-contacts
-        epiphany
-        geary
-        evolution
-        totem
-
-        # Игры
-        aisleriot
-        gnome-chess
-        gnome-mahjongg 
-        iagno
-        tali
-        hitori
-        atomix
-
-        # Документация
-        yelp
-        gnome-user-docs
-      ];
-      packages = with pkgs; [
-        gnome.gnome-tweaks
-
-        # Расширения
-        gnomeExtensions.bitcoin-markets
-        gnomeExtensions.blur-my-shell
-        gnomeExtensions.caffeine
-        gnomeExtensions.clipboard-history
-        gnomeExtensions.dash-to-dock
-        gnomeExtensions.desktop-cube
-        gnomeExtensions.search-light
-      ];
-    };
+    desktopManager.gnome.enable = true;
   };
+
+  environment.gnome.excludePackages = (with pkgs; [
+    # Ненужные приложения
+    gnome-tour
+    gnome-music
+    gnome-photos
+    gnome-contacts
+    epiphany
+    geary
+    evolution
+    totem
+
+    # Игры
+    aisleriot
+    gnome-chess
+    gnome-mahjongg 
+    iagno
+    tali
+    hitori
+    atomix
+
+    # Документация
+    yelp
+    gnome-user-docs
+  ]);
+
+  environment.systemPackages = with pkgs; [
+    gnome.gnome-tweaks
+
+    # Расширения
+    gnomeExtensions.bitcoin-markets
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.caffeine
+    gnomeExtensions.clipboard-history
+    gnomeExtensions.dash-to-dock
+    gnomeExtensions.desktop-cube
+    gnomeExtensions.search-light
+  ];
 
   programs.dconf.enable = true;
 }
