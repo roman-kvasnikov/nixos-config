46a6fd531dfb3cfb4960fdc42a9b74930e9ec0fe
Roman Kvasnikov <roman.kvasnikov@gmail.com>
2025-07-18 22:50:18 +0300

    Restructured

--- nixos/modules/fonts.nix
+++ nixos/modules/fonts.nix
@@ -7,13 +7,11 @@
     packages = with pkgs; [
       cascadia-code
       dina-font
-      fira-code
       font-awesome
       nerd-fonts.fira-code
       nerd-fonts.droid-sans-mono
       noto-fonts
       noto-fonts-emoji
-      noto-fonts-cjk-sans
       proggyfonts
       ubuntu_font_family
       liberation_ttf
