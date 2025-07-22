3afe1c4a9718eb03fa7fb0a06592b002741cf6e9
Roman Kvasnikov <roman.kvasnikov@gmail.com>
2025-07-22 19:07:34 +0300

    Restructured

--- home-manager/modules/code-cursor/default.nix
+++ home-manager/modules/code-cursor/default.nix
@@ -1,26 +1,26 @@
 { pkgs, user, ... }:
 
 let
-  vscodeSettings = builtins.fromJSON (
-    builtins.readFile "./settings.json"
-  );
+  # vscodeSettings = builtins.fromJSON (
+  #   builtins.readFile "./settings.json"
+  # );
 in {
   programs.vscode = {
     enable = true;
     package = pkgs.code-cursor;
-    # extensions = with pkgs.vscode-extensions; [
-    #   saeed-nazari.adonis-theme
-    #   formulahendry.auto-close-tag
-    #   formulahendry.auto-rename-tag
-    #   ms-vscode.azure-repos
-    #   github.remotehub
-    #   github.github-vscode-theme
-    #   jnoortheen.nix-ide
-    #   ms-vscode-remote.remote-ssh
-    #   ms-vscode-remote.remote-ssh-edit
-    #   ms-vscode.remote-explorer
-    #   ms-vscode.remote-repositories
-    # ];
-    userSettings = vscodeSettings;
+    extensions = with pkgs.vscode-extensions; [
+      saeed-nazari.adonis-theme
+      formulahendry.auto-close-tag
+      formulahendry.auto-rename-tag
+      ms-vscode.azure-repos
+      github.remotehub
+      github.github-vscode-theme
+      jnoortheen.nix-ide
+      ms-vscode-remote.remote-ssh
+      ms-vscode-remote.remote-ssh-edit
+      ms-vscode.remote-explorer
+      ms-vscode.remote-repositories
+    ];
+    # userSettings = vscodeSettings;
   };
 }
