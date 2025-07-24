c9fa87062dc2c667df3d46504fbc61695b670b65
Roman Kvasnikov <roman.kvasnikov@gmail.com>
2025-07-24 20:08:15 +0300

    Restructured

--- flake.nix
+++ flake.nix
@@ -34,6 +34,8 @@
 
           home-manager.nixosModules.home-manager
           {
+            inherit user;
+
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
