a42295ebd924bdac13ac232166af50fe3512a98b
Roman Kvasnikov <roman.kvasnikov@gmail.com>
2025-07-24 21:13:50 +0300

    Restructured

--- home-manager/modules/code-cursor/default.nix
+++ home-manager/modules/code-cursor/default.nix
@@ -10,26 +10,28 @@ in {
 
     package = pkgs.code-cursor;
 
-    enableUpdateCheck = false;
-
     # userSettings = vscodeSettings;
 
-    extensions = with pkgs.vscode-extensions; [
-      bbenoist.nix # Nix language support
-      esbenp.prettier-vscode # General formatting plugin
-      # open-southeners.laravel-pint
-      # shufo.vscode-blade-formatter
-      # saeed-nazari.adonis-theme
-      # formulahendry.auto-close-tag
-      # formulahendry.auto-rename-tag
-      # ms-vscode.azure-repos
-      # github.remotehub
-      # github.github-vscode-theme
-      # jnoortheen.nix-ide
-      # ms-vscode-remote.remote-ssh
-      # ms-vscode-remote.remote-ssh-edit
-      # ms-vscode.remote-explorer
-      # ms-vscode.remote-repositories
-    ];
+    profiles.default = {
+      enableUpdateCheck = false;
+
+      extensions = with pkgs.vscode-extensions; [
+        bbenoist.nix # Nix language support
+        esbenp.prettier-vscode # General formatting plugin
+        # open-southeners.laravel-pint
+        # shufo.vscode-blade-formatter
+        # saeed-nazari.adonis-theme
+        # formulahendry.auto-close-tag
+        # formulahendry.auto-rename-tag
+        # ms-vscode.azure-repos
+        # github.remotehub
+        # github.github-vscode-theme
+        # jnoortheen.nix-ide
+        # ms-vscode-remote.remote-ssh
+        # ms-vscode-remote.remote-ssh-edit
+        # ms-vscode.remote-explorer
+        # ms-vscode.remote-repositories
+      ];
+    };
   };
 }
