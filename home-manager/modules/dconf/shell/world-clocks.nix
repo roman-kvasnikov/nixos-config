b0304df0553d2839c62dc68578dcd5bb2494fcae
Roman Kvasnikov <roman.kvasnikov@gmail.com>
2025-07-25 11:42:20 +0300

    Restructured

--- home-manager/modules/dconf/shell/world-clocks.nix
+++ home-manager/modules/dconf/shell/world-clocks.nix
@@ -1,11 +1,34 @@
 { lib, ... }:
 
 {
+  # Не работает, нужно разобраться с форматом указания локации
   dconf.settings = {
-    "org/gnome/shell/world-clocks" = {
-      locations = lib.mkForce [
-        "@a(uv) [<(2, <('Dubai', 'OMDB', true, [(0.44069563612856821, 0.96574884695243146)], [(0.44073441734454749, 0.9648180105024653)])>)>"
-        "@a(uv) [<(2, <('Bangkok', 'VTBD', true, [(0.24289166005364171, 1.7558012275062955)], [(0.23998277214922031, 1.754346792280731)])>)>"
+    "org/gnome/clocks" = {
+      world-clocks = [
+        ([ 
+          (mkDictionaryEntry ["location" (mkVariant (mkTuple [
+            (mkUint32 2)
+            (mkVariant (mkTuple [
+              "Dubai"
+              "OMDB"
+              true
+              [(mkTuple [(0.44069563612856821) (0.96574884695243146)])]
+              [(mkTuple [(0.44073441734454749) (0.9648180105024653)])]
+            ]))
+          ]))])
+        ])
+        ([ 
+          (mkDictionaryEntry ["location" (mkVariant (mkTuple [
+            (mkUint32 2)
+            (mkVariant (mkTuple [
+              "Bangkok"
+              "VTBD"
+              true
+              [(mkTuple [(0.24289166005364171) (1.7558012275062955)])]
+              [(mkTuple [(0.23998277214922031) (1.754346792280731)])]
+            ]))
+          ]))])
+        ])
       ];
     };
   };
