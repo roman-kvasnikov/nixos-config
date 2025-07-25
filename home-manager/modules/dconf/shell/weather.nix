bb0922f7a0c8e2f63eb7b94b6ef4d56a2b4b15a6
Roman Kvasnikov <roman.kvasnikov@gmail.com>
2025-07-25 11:29:08 +0300

    Restructured

--- home-manager/modules/dconf/shell/weather.nix
+++ home-manager/modules/dconf/shell/weather.nix
@@ -1,9 +1,11 @@
+{ lib, ... }:
+
 {
   # Не работает, нужно разобраться с форматом указания локации
   dconf.settings = {
     "org/gnome/shell/weather" = {
       automatic-location = false;
-      locations = pkgs.lib.mkForce [
+      locations = lib.mkForce [
         "[<(uint32 2, <('Moscow', 'UUWW', true, [(0.97127572873484425, 0.65042604039431762)], [(0.97305983920281813, 0.65651530216830811)])>)>]"
       ];
     };
