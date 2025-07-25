3d2f46c522cdc934f4f500c75e16a2ffdd1e053f
Roman Kvasnikov <roman.kvasnikov@gmail.com>
2025-07-25 12:31:42 +0300

    Restructured

--- home-manager/modules/dconf/shell/world-clocks.nix
+++ home-manager/modules/dconf/shell/world-clocks.nix
@@ -3,27 +3,24 @@
 {
   # Не работает, нужно разобраться с форматом указания локации
   dconf.settings = {
-
-
-
-      settings = let
-        inherit (lib.gvariant) mkTuple mkUint32 mkVariant;
-      in {
-        "org/gnome/clocks" = {
-          world-clocks = [
-            {
-              location = mkVariant [(mkUint32 2) (mkVariant ["Oslo" "ENGM" false [(mkTuple [1.0506882097005865 0.19344065294494067])] [(mkTuple [1.0506882097005865 0.19344065294494067])]])];
-            }
-            {
-              location = mkVariant [(mkUint32 2) (mkVariant ["London" "EGWU" false [(mkTuple [0.8997172294030767 (-7.272211034407213e-3)])] [(mkTuple [0.8997172294030767 (-7.272211034407213e-3)])]])];
-            }
-            {
-              location = mkVariant [(mkUint32 2) (mkVariant ["Sydney" "YSSY" true [(mkTuple [(-0.592539281052075) 2.638646934988996])] [(mkTuple [(-0.5913757223996479) 2.639228723041856])]])];
-            }
-          ];
-        };
+    settings = let
+      inherit (lib.gvariant) mkTuple mkUint32 mkVariant;
+    in {
+      "org/gnome/clocks" = {
+        world-clocks = [
+          {
+            location = mkVariant [(mkUint32 2) (mkVariant ["Oslo" "ENGM" false [(mkTuple [1.0506882097005865 0.19344065294494067])] [(mkTuple [1.0506882097005865 0.19344065294494067])]])];
+          }
+          {
+            location = mkVariant [(mkUint32 2) (mkVariant ["London" "EGWU" false [(mkTuple [0.8997172294030767 (-7.272211034407213e-3)])] [(mkTuple [0.8997172294030767 (-7.272211034407213e-3)])]])];
+          }
+          {
+            location = mkVariant [(mkUint32 2) (mkVariant ["Sydney" "YSSY" true [(mkTuple [(-0.592539281052075) 2.638646934988996])] [(mkTuple [(-0.5913757223996479) 2.639228723041856])]])];
+          }
+        ];
       };
-    }
+    };
+}
 
 
 
@@ -37,4 +34,4 @@
     #   ];
     # };
   # };
-}
+# }
