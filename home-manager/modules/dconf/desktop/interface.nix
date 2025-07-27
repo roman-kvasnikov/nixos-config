f5b5b45e4a0fa57d73138dc66bcedfed71962129
RomanK <roman.kvasnikov@gmail.com>
2025-07-27 08:25:55 +0000

    Fixes

--- home-manager/modules/dconf/desktop/interface.nix
+++ home-manager/modules/dconf/desktop/interface.nix
@@ -5,9 +5,9 @@
       clock-format = "24h";
       clock-show-weekday = true;
       # color-scheme = "prefer-dark";
-      font-name = "FiraCode Nerd Font 11";
-      document-font-name = "FiraCode Nerd Font Light 11";
-      monospace-font-name = "FiraCode Nerd Font Mono 11";
+      # font-name = "FiraCode Nerd Font 11";
+      # document-font-name = "FiraCode Nerd Font Light 11";
+      # monospace-font-name = "FiraCode Nerd Font Mono 11";
       overlay-scrolling = true;
       show-battery-percentage = true;
     };
