9a8813f27362c7b5c33c4772ff719387e9e64a96
RomanK <roman.kvasnikov@gmail.com>
2025-08-08 09:12:58 +0000

    weather check

--- home-manager/modules/dconf/shell/weather.nix
+++ home-manager/modules/dconf/shell/weather.nix
@@ -1,10 +1,19 @@
-{lib, ...}: {
-  # Не работает, нужно разобраться с форматом указания локации
+{lib, ...}:
+with lib.hm.gvariant; {
   dconf.settings = {
     "org/gnome/shell/weather" = {
       automatic-location = false;
-      locations = lib.mkForce [
-        "[<(uint32 2, <('Moscow', 'UUWW', true, [(0.97127572873484425, 0.65042604039431762)], [(0.97305983920281813, 0.65651530216830811)])>)>]"
+      locations = mkArray type.variant [
+        (mkTuple [
+          (mkUint32 2)
+          (mkTuple [
+            "Moscow"
+            "UUWW" 
+            true
+            [(mkTuple [0.97127572873484425 0.65042604039431762])]
+            [(mkTuple [0.97305983920281813 0.65651530216830811])]
+          ])
+        ])
       ];
     };
   };
