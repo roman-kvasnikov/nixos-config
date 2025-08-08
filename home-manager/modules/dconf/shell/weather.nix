ef56fcb6e83ab9d3eaccada422e9688659807546
RomanK <roman.kvasnikov@gmail.com>
2025-08-08 09:19:21 +0000

    weather check

--- home-manager/modules/dconf/shell/weather.nix
+++ home-manager/modules/dconf/shell/weather.nix
@@ -1,23 +1,15 @@
-{lib, ...}:
-with lib.hm.gvariant; {
+{lib, ...}: {
   dconf.settings = {
     "org/gnome/shell/weather" = {
       automatic-location = false;
-      locations = mkVariant [
-        (mkVariant (mkTuple [
-          (mkUint32 2)
-          (mkVariant (mkTuple [
-            "Moscow"
-            "UUWW"
-            true
-            [
-              (mkTuple [0.9712757287348443 0.6504260403943177])
-            ]
-            [
-              (mkTuple [0.9730598392028181 0.6565153021683081])
-            ]
-          ]))
-        ]))
+      # Используем простой подход - через gsettings можно будет настроить вручную
+      # Или попробуем этот формат:
+    };
+    
+    # Альтернативно можно настроить через org/gnome/Weather напрямую
+    "org/gnome/Weather" = {
+      locations = [
+        "Moscow UUWW 55.752220 37.615555"
       ];
     };
   };
