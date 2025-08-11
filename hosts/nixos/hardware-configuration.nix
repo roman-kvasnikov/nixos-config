c8e760857b095944bbcea84dc0be7254e466296a
RomanK <roman.kvasnikov@gmail.com>
2025-08-11 17:08:23 +0000

    Добавлена конфигурация зашифрованного swap раздела

--- hosts/nixos/hardware-configuration.nix
+++ hosts/nixos/hardware-configuration.nix
@@ -29,7 +29,12 @@
       fsType = "ext4";
     };
 
-  swapDevices = [ ];
+  swapDevices = [
+    {
+      device = "/dev/mapper/nixos--vg-swap";
+      randomEncryption.enable = true;
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
