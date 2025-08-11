0da6fda00975c3f0c160d8ad4fdcb2ec9b5c2e3f
RomanK <roman.kvasnikov@gmail.com>
2025-08-11 15:14:14 +0000

    disko-luks

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -11,6 +11,14 @@
     ../../nixos/packages.nix
   ];
 
+  # Настройка LUKS
+  boot.initrd.luks.devices = {
+    "crypted" = {
+      device = "/dev/disk/by-uuid/a6cea734-ab18-47c0-b130-e922ba9d678b"; # UUID зашифрованного раздела!
+      preLVM = true; # LUKS расшифровывается ДО активации LVM
+    };
+  };
+
   services.openssh = {
     enable = false;
     settings = {
