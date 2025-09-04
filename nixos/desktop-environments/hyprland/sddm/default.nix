f6d4e180f83c882abe2feed5e14f164203bc424e
RomanK <roman.kvasnikov@gmail.com>
2025-09-04 19:05:27 +0300

    2025-09-04 19:05:27

--- nixos/desktop-environments/hyprland/sddm/default.nix
+++ nixos/desktop-environments/hyprland/sddm/default.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   environment.systemPackages = with pkgs; [
     libsForQt5.qt5.qtquickcontrols2
     libsForQt5.qt5.qtgraphicaleffects
@@ -9,6 +13,6 @@
 
     wayland.enable = true;
 
-    theme = "${import ./sddm-theme.nix {inherit pkgs;}}";
+    theme = "${import ./sddm-chili.nix {inherit pkgs inputs;}}";
   };
 }
