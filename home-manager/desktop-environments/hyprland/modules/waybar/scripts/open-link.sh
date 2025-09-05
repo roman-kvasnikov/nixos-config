32cbb09111b3af04d785f1c427399cbde4916941
RomanK <roman.kvasnikov@gmail.com>
2025-09-05 20:42:33 +0300

    2025-09-05 20:42:33

--- home-manager/desktop-environments/hyprland/modules/waybar/scripts/open-link.sh
+++ home-manager/desktop-environments/hyprland/modules/waybar/scripts/open-link.sh
@@ -6,4 +6,8 @@ xdg-open "$URL"
 
 sleep 1.0
 
-hyprctl dispatch focuswindow class:brave-browser
+BROWSER_WORKSPACE=$(hyprctl clients -j | jq -r '.[] | select(.class == "brave-browser") | .workspace.id' | head -1)
+
+if [ ! -z "$BROWSER_WORKSPACE" ]; then
+  hyprctl dispatch workspace "$BROWSER_WORKSPACE"
+fi
